689bd382e4550e18e04cdaa5432862b6e411ace6
wya <13782246806@163.com>
2020-04-09 10:50:10 +0800

    redis及微信登录

--- src/main/java/com/ecs/WXController/WXLoginController.java
+++ src/main/java/com/ecs/WXController/WXLoginController.java
@@ -1,15 +1,21 @@
 package com.ecs.WXController;
 
-
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.ecs.common.EncryptUtil;
 import com.ecs.common.HttpClientUtil;
 import com.ecs.common.JsonUtils;
 import com.ecs.common.MyParams;
@@ -17,20 +23,34 @@ import com.ecs.domain.WXSession;
 
 @Controller
 public class WXLoginController {
-	
+
 	@Autowired
-	MyParams myParams;
+	private StringRedisTemplate redis1StringRedisTemplate;
+
+	@Autowired
+	private MyParams myParams;
+
+	private EncryptUtil encryptUtil;
 	
+	@GetMapping("redis")
 	@ResponseBody
-	@RequestMapping(value="/wxLogin", method=RequestMethod.POST)
-	public String wxLogin (String code) {
+	@RequestMapping(value = "/wxLogin")
+	public Map<String, Object> wxLogin(String code,String session_id) {
+		
+		Map<String, Object> res = new HashMap<String, Object>();
+		
+		if(redis1StringRedisTemplate.hasKey(session_id)) {
+			System.out.println("111");
+		}else {
+			System.out.println("222");
+		}
 		
 //		https://api.weixin.qq.com/sns/jscode2session?
 //		appid=APPID&
 //		secret=SECRET&
 //		js_code=JSCODE&
 //		grant_type=authorization_code
-		
+
 		String url = "https://api.weixin.qq.com/sns/jscode2session";
 		Map<String, String> param = new HashMap<>();
 		String appid = myParams.getAppid();
@@ -40,14 +60,28 @@ public class WXLoginController {
 		param.put("secret", secret);
 		param.put("js_code", code);
 		param.put("grant_type", "authorization_code");
-		
+
 		String wxResult = HttpClientUtil.doGet(url, param);
-		System.out.println(wxResult);
+//		System.out.println(wxResult);
+
+		WXSession wxSession = JsonUtils.jsonToPoJo(wxResult, WXSession.class);
+//		System.out.println(wxSession.getOpenid());
+//		System.out.println(wxSession.getSession_key());
+
+		String sessionId = encryptUtil.encodeMD5Hex(wxSession.getOpenid());
+		String value = wxSession.getOpenid()+"&"+wxSession.getSession_key();
+//		System.out.println(sessionId);
+//		System.out.println(value);
 		
-		WXSession wxSession = JsonUtils.jsonToPoJo(wxResult, WXSession.class);		
-		System.out.println(wxSession.getOpenid());
-		System.out.println(wxSession.getSession_key());
 		
-		return "wya";
+		//参数: key,value,时间,时间单位（默认毫秒）
+//		redis1StringRedisTemplate.opsForValue().set(sessionId, value, 1, TimeUnit.DAYS);
+//		System.out.println(redis1StringRedisTemplate.opsForValue().get(sessionId));
+		
+		res.put("msg", "success");
+		res.put("sessionId", sessionId);
+		
+		
+		return res;
 	}
 }
--- src/main/java/com/ecs/domain/WXSession.java
+++ src/main/java/com/ecs/domain/WXSession.java
@@ -4,6 +4,10 @@ public class WXSession {
 
 	private String session_key;
 	private String openid;
+	private String unionid;
+	private Number errcode;
+	private String errmsg;
+	
 	
 	public String getSession_key() {
 		return session_key;
@@ -17,6 +21,23 @@ public class WXSession {
 	public void setOpenid(String openid) {
 		this.openid = openid;
 	}
-	
+	public String getUnionid() {
+		return unionid;
+	}
+	public void setUnionid(String unionid) {
+		this.unionid = unionid;
+	}
+	public Number getErrcode() {
+		return errcode;
+	}
+	public void setErrcode(Number errcode) {
+		this.errcode = errcode;
+	}
+	public String getErrmsg() {
+		return errmsg;
+	}
+	public void setErrmsg(String errmsg) {
+		this.errmsg = errmsg;
+	}
 	
 }
