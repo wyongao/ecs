38ba2b3e46011bd1d101c18e25cb9ae25924d182
wya <13782246806@163.com>
2020-06-07 19:56:40 +0800

    上传圈圈

--- src/main/java/com/ecs/EcsApplication.java
+++ src/main/java/com/ecs/EcsApplication.java
@@ -8,14 +8,14 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 // extends SpringBootServletInitializer
 
 @SpringBootApplication
-public class EcsApplication extends SpringBootServletInitializer{
+public class EcsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcsApplication.class, args);
 	}
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(EcsApplication.class);
-	}
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//		return builder.sources(EcsApplication.class);
+//	}
 }
